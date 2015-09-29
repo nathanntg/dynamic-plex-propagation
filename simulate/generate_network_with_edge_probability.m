dfbd05ece06c2f809f77d7284a489f351139d2d0
Nathan Perkins <nathan@maxmo.net>
2015-09-29 08:33:07 -0400

    Better documentation.

--- simulate/generate_network_with_edge_probability.m
+++ simulate/generate_network_with_edge_probability.m
@@ -1,13 +1,14 @@
 function C = generate_network_with_edge_probability(p_edge, time_steps)
-%GENERATE_NETWORK_WITH_EDGE_PROBABILITY Summary of this function goes here
+%GENERATE_NETWORK_WITH_EDGE_PROBABILITY Network based on edge probabilities
 %   Generates a dynamic network based on the probability of edges existing
 %   between two vertices.
 %
-%   C = generate_network(P_EDGE, TIME_STEPS) takes a square (N x N),
-%   symmetric matrix P_EDGE representing the probability of edges existing 
-%   between two vertices (diagonal elements are ignored) and produces a
-%   logical dynamic network of size N x N x TIME_STEPS representing random
-%   connectivity chosen independently for each edge and each time step.
+%   C = generate_network_with_edge_probability(P_EDGE, TIME_STEPS) takes a 
+%   square (N x N), symmetric matrix P_EDGE representing the probability of 
+%   edges existing between two vertices (diagonal elements are ignored) and 
+%   produces a logical dynamic network of size N x N x TIME_STEPS 
+%   representing random connectivity chosen independently for each edge and 
+%   each time step.
 
 % number of vertices
 n = size(p_edge, 1);
