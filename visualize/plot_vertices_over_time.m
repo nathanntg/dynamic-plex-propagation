dc9f21389afbf7463c353416389b2763f3c225e2
Nathan Perkins <nathan@maxmo.net>
2015-11-13 20:24:36 -0500

    Optimize plotting of vertices over time.

--- visualize/plot_vertices_over_time.m
+++ visualize/plot_vertices_over_time.m
@@ -10,29 +10,41 @@ end
 % extract list of all dynamic communities
 all_dyn_comms = all_dynamic_communities(dynamic_communities);
 
-% ages
+% number of dynamic communities
 num_dynamic_communities = length(all_dyn_comms);
+
+% make an index
+index = cell(1, max(all_dyn_comms));
+for t = 1:length(dynamic_communities)
+    % current dynamic communities
+    c = dynamic_communities{t};
+
+    % add time step to index
+    if ~isempty(c)
+        index(c) = cellfun(@(x) [x t], index(c), 'UniformOutput', false);
+    end
+end
+
+% make cell array for plot function
+ca = cell(1, 2 * num_dynamic_communities);
 
 % calculate timeline for each dynamic community
 for i = 1:num_dynamic_communities
     cur_dynamic_comm = all_dyn_comms(i);
-    cur_vertices = vertices_in_community(vertices, dynamic_communities, cur_dynamic_comm);
+    idx = index{cur_dynamic_comm};
+    cur_vertices = vertices_in_community(vertices(idx), dynamic_communities(idx), cur_dynamic_comm);
     num = sum(cur_vertices, 2);
     
-    % non-zero time indices
-    nz = num > 0;
-    
-    % plot
-    if 1 < i
-        hold on;
-        plot(time_axis(nz), num(nz));
-        hold off;
-    else
-        plot(time_axis(nz), num(nz));
-    end
+    % add to cell array
+    ca{i * 2 - 1} = time_axis(idx);
+    ca{i * 2} = num;
 end
 
+% free up memory
+clear index;
+
 % configure plot
+plot(ca{:});
 xlim([time_axis(1) time_axis(end)]);
 xlabel('Time');
 ylabel('Number of Vertices');
